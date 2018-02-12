dde68b3ccdda6786385a84cffdc7ad45f36aba7a
Satendra Kumar Kushwaha <Satendra1729@gmail.com>
2018-02-12 21:51:50 +0530

    First commit

---
+++ SingletonPatternChecking/DummyMapperQualifierHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading; 
+
+namespace SingletonPatternChecking
+{
+    class DummyMapperQualifierHelper
+    {
+        static object loc = new object();
+        public static string BillMapper;
+
+        private static List<string> MapperQualifiers = new List<string>();
+
+        private static readonly DummyMapperQualifierHelper mqh = new DummyMapperQualifierHelper();
+
+        private DummyMapperQualifierHelper() { }
+
+        public static DummyMapperQualifierHelper GetInstance(string name,object loc)
+        {
+            //var billMapperService = DependencyResolver.Current.GetService<IBillMapperService>();
+            lock (DummyMapperQualifierHelper.loc)
+            {
+                BillMapper = name;
+                //var billMapper = billMapperService.GetBillMapperByMapperName(BillMapper);
+                MapperQualifiers = new List<string> { name + "qualifierList" };
+                
+                Console.WriteLine("Bill Mapper Name {0} Thread Name : {1} Qualifire{2}",BillMapper,name, MapperQualifiers[0]);
+            }
+                Thread.Sleep(10000);
+            Console.WriteLine("After lock and sleep assigned value in this class will change and reflect only last value but not passed ");
+            Console.WriteLine("Bill Mapper Name {0}|| Thread Name : {1} || Qualifire {2}", BillMapper, name, MapperQualifiers[0]);
+            return mqh;
+            //}
+        }
+
+    }
+}
---
+++ SingletonPatternChecking/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+namespace SingletonPatternChecking
+{
+    class Program
+    {
+        static object loc = new object();
+        public static List<Thread> ths = new List<Thread>();
+        static void Main(string[] args)
+        {
+            object loc = new object();
+            for (int i = 0; i < 10; i++)
+            {
+                int j = i;
+                ths.Add(new Thread(() => DummyMapperQualifierHelper.GetInstance("worker"+j.ToString(), loc)));
+            }
+            foreach (Thread th in ths)
+                th.Start();
+            Console.ReadLine(); 
+        }
+    }
+}
